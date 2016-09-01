1dc326b23b1f2d95690a68513f4efc2763945c30
Susan Thomson <sthomson@scottlogic.co.uk>
2016-09-01 11:44:03 +0100

    bundling with webpack

---
+++ public/chatApp.js
@@ -0,0 +1,5 @@
+require("./js/main.js");
+require("./js/chatService.js");
+require("./js/ConversationController.js");
+require("./js/UserListController.js");
+require("./js/ConversationListController.js");
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: "./public/chatApp.js",
+  output: {
+    path: "./public",
+    filename: "bundle.js"
+  }
+}
