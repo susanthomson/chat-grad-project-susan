c5201971c7266252a2816e7ac43f3c89e27a7042
Susan Thomson <sthomson@scottlogic.co.uk>
2016-09-09 10:32:07 +0100

    speech bubbles

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,14 @@ module.exports = {
   output: {
     path: "./public",
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      }
+    ]
   }
 };
