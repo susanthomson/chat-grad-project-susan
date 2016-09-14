479391d9761922e73694417b9ce2b589972e64c4
Susan Thomson <sthomson@scottlogic.co.uk>
2016-09-14 16:46:39 +0100

    show profile

--- public/app/ChatController.js
+++ public/app/ChatController.js
@@ -1,13 +1,38 @@
 (function() {
     var app = angular.module("ChatApp");
 
-    app.controller("ChatController", ["$scope", "$http", "chatService",
-    function($scope, $http, chatService) {
+    app.controller("ChatController", ["$scope", "$http", "chatService", "$mdPanel",
+    function($scope, $http, chatService, $mdPanel) {
         $scope.loggedIn = false;
 
         $http.get("/api/user").then(function(userResult) {
             $scope.loggedIn = true;
             $scope.user = userResult.data;
+
+            var _mdPanel = $mdPanel;
+            $scope.showProfile = function () {
+                var position = _mdPanel.newPanelPosition()
+                    .absolute()
+                    .center();
+
+                var config = {
+                    attachTo: angular.element(document.body),
+                    controller: PanelDialogCtrl,
+                    controllerAs: 'ctrl',
+                    disableParentScroll: false,
+                    templateUrl: 'panel.tmpl.html',
+                    hasBackdrop: true,
+                    panelClass: 'demo-dialog-example',
+                    position: position,
+                    trapFocus: true,
+                    zIndex: 150,
+                    clickOutsideToClose: true,
+                    escapeToClose: true,
+                    focusOnOpen: true
+                };
+
+                _mdPanel.open(config);
+            };
 
             $scope.updateConversation = function () {
                 if (chatService.activeConversationId) {
@@ -30,6 +55,21 @@
                 $scope.loginUri = result.data.uri;
             });
         });
-    }]);
+    }])
+    .controller('PanelDialogCtrl', PanelDialogCtrl);
+
+    function PanelDialogCtrl(mdPanelRef) {
+        this._mdPanelRef = mdPanelRef;
+    }
+
+    PanelDialogCtrl.prototype.closeDialog = function() {
+    
+        var panelRef = this._mdPanelRef;
+
+        panelRef && panelRef.close().then(function() {
+            angular.element(document.querySelector('.demo-dialog-open-button')).focus();
+            panelRef.destroy();
+        });
+    };
 
 })();
